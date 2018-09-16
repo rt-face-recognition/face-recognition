69fa96c5e5fbbdddb2ca1a68485190155f0daab1
Ethan Lee <nathe.eel@gmail.com>
2018-09-16 03:28:29 -0400

    changes

--- src/restful.py
+++ src/restful.py
@@ -5,41 +5,33 @@ from flask import request
 
 app = Flask(__name__)
 
-tasks = [
+faces = [
     {
         'id': 1,
         'title': u'Buy groceries',
         'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
         'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
     }
 ]
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
+@app.route('/facial/api/v1.0/faces', methods=['GET'])
+def get_faces():
+    return jsonify({'faces': faces})
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-@app.route('/todo/api/v1.0/tasks', methods=['POST'])
-def create_task():
-    if not request.json or not 'title' in request.json:
+@app.route('/facial/api/v1.0/faces', methods=['POST'])
+def create_face():
+    if not request.json or not 'faces' in request.json:
         abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
+    face = {
+        'name': request.json['name'],
+        'date': request.json['date']
     }
-    tasks.append(task)
-    return jsonify({'task': task}), 201
+    faces.append(face)
+    return jsonify({'face': face}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
